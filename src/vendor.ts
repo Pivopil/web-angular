77b5fff4129d0c9ccbd0eb813e861b9d86a2d3df
o.aleksandrov <o.aleksandrov@aimprosoft.com>
2017-01-31 13:47:42 +0200

    initial commit

---
+++ src/vendor.ts
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap-theme.min.css';
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/compiler';
+import '@angular/http';
+import '@angular/router';
+import '@angular/forms';
+import 'rxjs';
